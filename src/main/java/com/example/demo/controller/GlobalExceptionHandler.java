4dce25383e01b4bc1ad2517bd1a3b2bda7d95dc5
deo23 <muhammad.deo.tif421@polban.ac.id>
2023-09-16 12:20:36 +0700

    Revise Exception Class

--- src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -27,21 +27,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 }
 
 
-// package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.context.request.WebRequest;
-
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
-
-//     @ExceptionHandler(Exception.class)
-//     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
-//         // You can customize the error response here
-//         String errorMessage = "An error occurred.";
-//         return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-//     }
-// }
