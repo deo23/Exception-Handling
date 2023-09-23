a7cbe8b3fa25e31f86c996cda1fb870c0c89f672
deo23 <muhammad.deo.tif421@polban.ac.id>
2023-09-23 13:06:05 +0700

    Add docker

--- src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -13,11 +13,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
         // Extract the exception message
-        String errorMessage = ex.getMessage();
-        if (errorMessage == null) {
-            errorMessage = ex.toString();
-        }
-        
+        String errorMessage = ex.getMessage();        
         
         // Return a simplified JSON response with the error message
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -27,4 +23,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 }
 
 
+// package com.example.demo.exception;
 
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.context.request.WebRequest;
+
+// @ControllerAdvice
+// public class GlobalExceptionHandler {
+
+//     @ExceptionHandler(Exception.class)
+//     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+//         // You can customize the error response here
+//         String errorMessage = "An error occurred.";
+//         return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+//     }
+// }
